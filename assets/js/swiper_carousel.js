f536b1d9160cafee3e5529e2eb9a0617fa530a44
siriusgurban <failoglu96@gmail.com>
2025-07-03 11:07:17 +0400

    about section added

--- assets/js/swiper_carousel.js
+++ assets/js/swiper_carousel.js
@@ -2,8 +2,11 @@ const swiper = new Swiper(".swiper", {
   // Optional parameters
   // direction: 'vertical',
   loop: true,
+  speed: 700,
   // slidesPerView: 1,
-  autoPlay: true,
+  autoplay: {
+    delay: 3000, // Delay between transitions in autoplay mode
+  },
   effect: "fade",
   fadeEffect: {
     crossFade: true,
